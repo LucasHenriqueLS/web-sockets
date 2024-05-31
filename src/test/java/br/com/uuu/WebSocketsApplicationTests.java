4fe11f0dd329f088d8e7fd76b19d13743aec997e
LucasHenriqueLS <lucashlsl100@gmail.com>
2024-05-31 13:56:26 -0300

    Project implemented

---
+++ src/test/java/br/com/uuu/WebSocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uuu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
